48c9f38952a9461d95ad4f17930bec0c03955ef6
CH4 <ch4-sylw@qq.com>
2026-06-15 22:20:40 +0800

    release version v1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "org.dpdns.sylw.videostreamer"
         minSdk = 29
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
